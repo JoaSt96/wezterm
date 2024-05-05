daca4d6f598ca34406a697cdcc85738b637ea202
JoaSt96 <joaquin.stok@hotmail.com>
2024-05-05 19:55:55 -0300

    wezterm config

---
+++ wezterm.lua
@@ -0,0 +1,25 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+local mux = wezterm.mux
+
+wezterm.on("gui-startup", function()
+  local tab, pane, window = mux.spawn_window{}
+  window:gui_window():maximize()
+end)
+
+config.font =
+  wezterm.font('JetBrains Mono', { weight = 'Bold' })
+config.color_scheme = 'Urple (Gogh)'
+config.font_size = 16.0
+config.window_background_opacity = 1.0
+config.audible_bell = 'Disabled'
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+config.native_macos_fullscreen_mode = true
+-- config.macos_window_background_blur = 20
+-- config.window_background_opacity = 0.3
+
+
+
+return config
